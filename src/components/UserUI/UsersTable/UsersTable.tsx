8e9987b01ed21b38dd8ec7593eb50f30b8296925
Sergio Ivanuzzo <sergio.ivanuzzo@gmail.com>
2019-11-23 00:11:49 +0200

    fixed warning causing by incorrect TableCell children type

--- src/components/UserUI/UsersTable/UsersTable.tsx
+++ src/components/UserUI/UsersTable/UsersTable.tsx
@@ -54,12 +54,24 @@ class UsersTable extends React.Component<IUsersTableProps> {
                 <TableCell component="th" scope="row">
                     {user.first_name}
                 </TableCell>
-                <TableCell align="right">{user.last_name}</TableCell>
-                <TableCell align="right">{user.birth_date}</TableCell>
-                <TableCell align="right">{user.gender}</TableCell>
-                <TableCell align="right">{user.job}</TableCell>
-                <TableCell align="right">{user.biography}</TableCell>
-                <TableCell align="right">{user.is_active}</TableCell>
+                <TableCell align="right">
+                    <span>{user.last_name}</span>
+                </TableCell>
+                <TableCell align="right">
+                    <span>{user.birth_date}</span>
+                </TableCell>
+                <TableCell align="right">
+                    <span>{user.gender}</span>
+                </TableCell>
+                <TableCell align="right">
+                    <span>{user.job}</span>
+                </TableCell>
+                <TableCell align="right">
+                    <span>{user.biography}</span>
+                </TableCell>
+                <TableCell align="right">
+                    <span>{user.is_active}</span>
+                </TableCell>
                 <TableCell align="right">
 
                 </TableCell>
